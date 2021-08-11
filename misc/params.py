37f116569ae4214d7bdd1ae4d36e4394e74a5903
Eric <tang8156@gmail.com>
2021-08-11 01:43:57 -0400

    change the gating threshold

--- misc/params.py
+++ misc/params.py
@@ -28,7 +28,7 @@ sigma_p66 = 5 # initial setting for estimation error covariance P entry for vz
 weight_dim = 0.1 # sliding average parameter for dimension estimation
 
 # association parameters (Step 3)
-gating_threshold = 0.995 # percentage of correct measurements that shall lie inside gate
+gating_threshold = 0.9999999999 # percentage of correct measurements that shall lie inside gate
 
 # measurement parameters (Step 4)
 sigma_lidar_x = 0.1 # measurement noise standard deviation for lidar x position   
